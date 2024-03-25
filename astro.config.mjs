ad86cf233ba4f24674a6d3cf14c1c5f4cf440712
Martin Juul Petersen <capsup@live.dk>
2024-03-26 02:41:40 +1100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://juul-it.github.io,
+	site: 'https://juul-it.github.io',
 	base: '/websites-hosting.juulit.com-guides-palworld',
 	integrations: [
 		starlight({
